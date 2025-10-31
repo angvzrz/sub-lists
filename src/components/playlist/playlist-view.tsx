404143c66c8ae54c03c67241a69e9ff2804648ce
Angel Valdez <ang_vzrz@live.com.mx>
2025-10-30 20:57:48 -0600

    fix: add semantic headers to distinguish sub-playlists and videos sections

--- src/components/playlist/playlist-view.tsx
+++ src/components/playlist/playlist-view.tsx
@@ -9,27 +9,33 @@ type PlaylistProps = Readonly<{
 export function PlaylistView({ playList }: PlaylistProps) {
   return (
     <>
-      <ul className="grid grid-cols-1 gap-4 p-8 sm:grid-cols-2 lg:grid-cols-3">
-        {playList.subPlaylists?.map((subPlaylist) => (
-          <li
-            key={subPlaylist.id}
-            className="flex flex-col rounded-md border p-4"
-          >
-            <SubPlaylistItem playlist={subPlaylist} />
-          </li>
-        ))}
-      </ul>
-      <ul className="grid grid-cols-1 gap-4 p-8">
-        {!playList.videos || playList.videos.length === 0 ? (
-          <div className="p-4">
-            <p className="text-gray-500">No videos in this playlist yet</p>
-          </div>
-        ) : (
-          playList.videos.map((video) => (
-            <PlaylistVideoItem key={video.id} video={video} />
-          ))
-        )}
-      </ul>
+      <section>
+        <h2 className="px-8 pt-8 text-xl font-bold">Sub-Playlists</h2>
+        <ul className="grid grid-cols-1 gap-4 p-8 sm:grid-cols-2 lg:grid-cols-3">
+          {playList.subPlaylists?.map((subPlaylist) => (
+            <li
+              key={subPlaylist.id}
+              className="flex flex-col rounded-md border p-4"
+            >
+              <SubPlaylistItem playlist={subPlaylist} />
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section>
+        <ul className="grid grid-cols-1 gap-4 p-8">
+          <h2 className="px-8 pt-8 text-xl font-bold">Videos</h2>
+          {!playList.videos || playList.videos.length === 0 ? (
+            <div className="p-4">
+              <p className="text-gray-500">No videos in this playlist yet</p>
+            </div>
+          ) : (
+            playList.videos.map((video) => (
+              <PlaylistVideoItem key={video.id} video={video} />
+            ))
+          )}
+        </ul>
+      </section>
     </>
   );
 }
