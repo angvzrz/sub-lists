8f075b39f475f41840b49fd20db5a48a1a3f1c3b
Angel Valdez <ang_vzrz@live.com.mx>
2025-11-03 20:04:41 -0600

    fix: remove redundant key item

--- src/components/playlist/playlist-video-item.tsx
+++ src/components/playlist/playlist-video-item.tsx
@@ -5,10 +5,10 @@ type PlaylistVideoProps = Readonly<{
 }>;
 
 export function PlaylistVideoItem({
-  video: { id, title, playlistId: playlist, channel },
+  video: { title, playlistId: playlist, channel },
 }: PlaylistVideoProps) {
   return (
-    <li key={id} className="flex flex-col rounded-md border p-4">
+    <li className="flex flex-col rounded-md border p-4">
       <h2 className="text-lg font-semibold">{title}</h2>
       <p className="text-sm text-gray-500">{channel}</p>
       <p className="text-sm text-gray-500">{playlist}</p>
