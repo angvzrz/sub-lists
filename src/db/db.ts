b003dd7fceb89ec872dd33b0e1ad0fef85402724
Angel Valdez <ang_vzrz@live.com.mx>
2025-11-07 23:59:25 -0600

    feat: add video schema to db

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,17 +1,19 @@
 // db.ts
 import Dexie, { type EntityTable } from 'dexie';
-import type { Playlist } from '../types';
+import type { Playlist, PlaylistVideo } from '../types';
 
 const db = new Dexie('FriendsDatabase') as Dexie & {
   playlists: EntityTable<
     Playlist,
     'id' // primary key "id" (for the typings only)
   >;
+  videos: EntityTable<PlaylistVideo, 'id'>;
 };
 
 // Schema declaration:
 db.version(1).stores({
   playlists: 'id, title, createdAt, updatedAt, category, subPlaylists, videos',
+  videos: 'id, title, playlistId, channel, createdAt, updatedAt',
 });
 
 export { db };
